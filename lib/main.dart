2001d3f3ea19c4663549d310dd55250374316f25
rosini132 <127768435+rosini132@users.noreply.github.com>
2024-05-01 10:11:13 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,28 @@
-import 'screens/note.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/adapters.dart';
-// import 'note.dart';// Adjusted import statement
+import 'screens/ModePage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+// import 'package:firebase_analytics/firebase_analytics.dart';
 
-// void main() async
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Initialize Firestore
 
-late Box<Note> box;
-
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();   //HIVE SETUP
-  await Hive.initFlutter();   //HIVE SETUP
-  Hive.registerAdapter(NoteAdapter());
-  box = await Hive.openBox<Note>('notes');
   runApp(MyApp());
+  // FirebaseFirestore.instance.settings =
+  //     const Settings(persistenceEnabled: true);
 }
 
 class MyApp extends StatelessWidget {
+  // final FirebaseFirestore
+  //     firestore; // Declare a variable to hold FirebaseFirestore instance
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(), // Adjusted reference to HomeScreen
+      home: ModePage(), // Adjusted reference to HomeScreen
     );
   }
 }
