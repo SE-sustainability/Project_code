14a6be1b080700120bf36de453c6845faee6ac6f
rosini132 <127768435+rosini132@users.noreply.github.com>
2024-05-07 03:12:59 +0100

    Update reminder.dart

--- lib/screens/Backend/reminder.dart
+++ lib/screens/Backend/reminder.dart
@@ -1,32 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Reminder {
   final String title;
   final bool completed;
+  final String description;
+  final Timestamp dateTime;
 
-  Reminder({
-    required this.title,
-    required this.completed,
-  });
+  Reminder(
+      {required this.title,
+      required this.completed,
+      required this.description,
+      required this.dateTime});
 
   Reminder.fromJson(Map<String, Object?> json)
       : this(
-          title: json['title']! as String,
-          completed: json['completed']! as bool,
-        );
+            title: json['title']! as String,
+            completed: json['completed']! as bool,
+            description: json['description'] as String,
+            dateTime: json['dateTime'] as Timestamp);
 
-  Reminder copyWith({
-    String? title,
-    bool? completed,
-  }) {
+  Reminder copyWith(
+      {String? title,
+      bool? completed,
+      String? description,
+      Timestamp? dateTime}) {
     return Reminder(
-      title: title ?? this.title,
-      completed: completed ?? this.completed,
-    );
+        title: title ?? this.title,
+        completed: completed ?? this.completed,
+        description: description ?? this.description,
+        dateTime: dateTime ?? this.dateTime);
   }
 
   Map<String, Object?> toJson() {
     return {
       'title': title,
       'completed': completed,
+      'description': description,
+      'dateTime': dateTime
     };
   }
 }
+
