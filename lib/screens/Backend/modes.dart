448e3739ae6ea160f792ac9ee7d42b1b66082a03
rosini132 <127768435+rosini132@users.noreply.github.com>
2024-05-09 11:05:03 +0100

    Update modes.dart

--- lib/screens/Backend/modes.dart
+++ lib/screens/Backend/modes.dart
@@ -1,21 +1,39 @@
-// import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart';
 class Mode {
-  final int id;
-  final String name;
-  final String icon;
-  final String colour;
+  final String id;
+  final String description;
+  final IconData icon;
+  final Color color;
 
-  Mode(
-      {required this.id,
-      required this.name,
-      required this.icon,
-      required this.colour});
+  Mode({required this.id,required this.description, required this.icon, required this.color});
 
-  Mode.fromJson(Map<String, Object?> json)
-      : this(
-            id: json['id']! as int,
-            name: json['name']! as String,
-            icon: json['icon']! as String,
-            colour: json['colour']! as String);
+  Mode.fromJson(Map<String, dynamic> json)
+      : id = json['id'] as String,
+        description = json['description'] as String,
+        icon = IconData(int.parse(json['icon'] as String), fontFamily: 'MaterialIcons'), // Convert icon code to IconData
+        color = Color(int.parse(json['color'] as String)
+        );
+
+  Mode copyWith({
+    String? id,
+    String? description,
+    IconData? icon,
+    Color? color,
+  }) {
+    return Mode(
+      id: id ?? this.id,
+      description: description ?? this.description,
+      icon: icon?? this.icon,
+      color: color?? this.color,
+
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'id' : id,
+      'description' : description,
+      'icon' : '${icon.codePoint}',
+      'color' : '${color.value}',
+    };
+  }
 }
